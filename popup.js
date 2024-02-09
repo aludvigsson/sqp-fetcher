b87dba17e7d666e13e9a0cbc352a657680aefa76
amistry007 <ashok_mistry@hotmail.com>
2024-02-09 18:08:23 +0000

    Update popup.js
    - Add storage of user input to allow recall of the data entered previously
    - Add checkbox for future enhancement of one consolidated CSV file
    - Add error handling in case user is not logged into Amazon

--- popup.js
+++ popup.js
@@ -1,12 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	// Reset error message
+	const errorMessage = document.getElementById("errorMessage");
+	errorMessage.innerText = "";
+	errorMessage.style.display = "none";
+
+    chrome.storage.local.get(['asinInput', 'startDate', 'endDate', 'isSingleFilePerAsin'], function(result) {
+        if (result.asinInput) {
+            document.getElementById('asinInput').value = result.asinInput;
+        }
+        if (result.startDate) {
+            document.getElementById('startDateInput').value = result.startDate;
+        }
+        if (result.endDate) {
+            document.getElementById('endDateInput').value = result.endDate;
+        }
+        if (result.isSingleFilePerAsin) {
+            document.getElementById('isSingleFilePerAsin').checked = result.isSingleFilePerAsin;
+        }
+        // Optionally, you could trigger data fetching automatically if the saved state indicates a process was ongoing.
+    });
+});
+
 document.getElementById('fetchData').addEventListener('click', function() {
+
+	// Reset progress message
+    const progressBar = document.getElementById("progressBar");
+    const progressText = document.getElementById("progressText");
+    progressBar.style.width = `0%`;
+    progressText.innerText = "";
+
+	// Reset error message
+	const errorMessage = document.getElementById("errorMessage");
+	errorMessage.innerText = "";
+	errorMessage.style.display = "none";
+
+	// Get user input
     const asinInput = document.getElementById('asinInput').value;
     const asins = asinInput.split(/\s+|,/).filter(asin => asin.trim() !== ''); // Split by whitespace or comma and filter out empty strings
     const startDate = document.getElementById('startDateInput').value;
     const endDate = document.getElementById('endDateInput').value;
     const marketplace = document.getElementById('marketplaceSuffix').innerText;
+	const isSingleFilePerAsin = document.getElementById('isSingleFilePerAsin').checked;
+
+	// Save to Chrome storage
+    chrome.storage.local.set({ asinInput, startDate, endDate, isSingleFilePerAsin }, function() {
+        console.log('SQP Fetcher input data saved');
+    });
 
     asins.forEach(asin => {
-        chrome.runtime.sendMessage({action: 'fetchData', asin: asin.trim(), startDate, endDate, marketplace});
+        chrome.runtime.sendMessage({action: 'fetchData', asin: asin.trim(), startDate, endDate, marketplace, isSingleFilePerAsin});
     });
 });
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -19,6 +62,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         const progressText = document.getElementById("progressText");
         progressBar.style.width = `${message.progress}%`;
         progressText.innerText = `ASIN ${message.ASIN}: Week ${message.week} - Progress ${message.progress}%`; // Update text with ASIN and week information
+    }
+    if (message.type === "ERROR") {
+		const errorMessage = document.getElementById("errorMessage");
+    	errorMessage.innerText = message.message;
+	    errorMessage.style.display = "block";
+	    errorMessage.style.backgroundColor = "red";
     }
 });
 
@@ -65,4 +114,3 @@ function updateMarketplaceDisplay(marketplaceSuffix) {
     const marketplaceDisplay = document.getElementById('marketplaceSuffix');
     marketplaceDisplay.innerText = marketplaceSuffix;
 }
-
