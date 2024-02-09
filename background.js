ea1cd1b0cdc982bfb527a5737b8beb7e2d608967
amistry007 <ashok_mistry@hotmail.com>
2024-02-09 12:03:45 +0000

    Update background.js
    - Revert wrong commit
    - Replace "-" with 0 in all columns
    - Escape single quotes and double quotes in the search query field
    - Fix issue with all ASIN data in one file by declaring localCSVData in function fetchAllData

--- background.js
+++ background.js
@@ -1,67 +1,195 @@
-document.getElementById('fetchData').addEventListener('click', function() {
-    const asinInput = document.getElementById('asinInput').value;
-    const asins = asinInput.split(/\s+|,/).filter(asin => asin.trim() !== ''); // Split by whitespace or comma and filter out empty strings
-    const startDate = document.getElementById('startDateInput').value;
-    const endDate = document.getElementById('endDateInput').value;
-    const marketplace = document.getElementById('marketplaceSuffix').innerText;
+const columnMapping = {
+    "qp-asin-query": "Search Query",
+    "qp-asin-query-rank": "Search Query Score",
+    "qp-asin-query-volume": "Search Query Volume",
+    "qp-asin-impressions": "Impressions: Total Count",
+    "qp-asin-count-impressions": "Impressions: ASIN Count",
+    "qp-asin-share-impressions": "Impressions: ASIN Share %",
+    "qp-asin-clicks": "Clicks: Total Count",
+    "qp-click-rate": "Clicks: Click Rate %",
+    "qp-asin-count-clicks": "Clicks: ASIN Count",
+    "qp-asin-share-clicks": "Clicks: ASIN Share %",
+    "qp-asin-median-query-price-clicks": "Clicks: Price (Median)",
+    "qp-asin-median-price-clicks": "Clicks: ASIN Price (Median)",
+    "qp-asin-same-day-shipping-clicks": "Clicks: Same Day Shipping Speed",
+    "qp-asin-one-day-shipping-clicks": "Clicks: 1D Shipping Speed",
+    "qp-asin-two-day-shipping-clicks": "Clicks: 2D Shipping Speed",
+    "qp-asin-cart-adds": "Cart Adds: Total Count",
+    "qp-asin-cart-add-rate": "Cart Adds: Cart Add Rate %",
+    "qp-asin-count-cart-adds": "Cart Adds: ASIN Count",
+    "qp-asin-share-cart-adds": "Cart Adds: ASIN Share %",
+    "qp-asin-median-query-price-cart-adds": "Cart Adds: Price (Median)",
+    "qp-asin-median-price-cart-adds": "Cart Adds: ASIN Price (Median)",
+    "qp-asin-same-day-shipping-cart-adds": "Cart Adds: Same Day Shipping Speed",
+    "qp-asin-one-day-shipping-cart-adds": "Cart Adds: 1D Shipping Speed",
+    "qp-asin-two-day-shipping-cart-adds": "Cart Adds: 2D Shipping Speed",
+    "qp-asin-purchases": "Purchases: Total Count",
+    "qp-asin-purchase-rate": "Purchases: Purchase Rate %",
+    "qp-asin-count-purchases": "Purchases: ASIN Count",
+    "qp-asin-share-purchases": "Purchases: ASIN Share %",
+    "qp-asin-median-query-price-purchases": "Purchases: Price (Median)",
+    "qp-asin-median-price-purchases": "Purchases: ASIN Price (Median)",
+    "qp-asin-same-day-shipping-purchases": "Purchases: Same Day Shipping Speed",
+    "qp-asin-one-day-shipping-purchases": "Purchases: 1D Shipping Speed",
+    "qp-asin-two-day-shipping-purchases": "Purchases: 2D Shipping Speed",
+    "week": "Week",
+    "asin": "ASIN",
+    "marketplace": "Marketplace"
+};
 
-    asins.forEach(asin => {
-        chrome.runtime.sendMessage({action: 'fetchData', asin: asin.trim(), startDate, endDate, marketplace});
-    });
-});
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "CSV_DATA") {
-        downloadCSV(message.payload, message.startDate, message.endDate, message.ASIN);
-    }
-    if (message.type === 'UPDATE_PROGRESS') {
-        console.log(`Received progress for ASIN ${message.ASIN}: ${message.progress}%`);
-        const progressBar = document.getElementById("progressBar");
-        const progressText = document.getElementById("progressText");
-        progressBar.style.width = `${message.progress}%`;
-        progressText.innerText = `ASIN ${message.ASIN}: Week ${message.week} - Progress ${message.progress}%`; // Update text with ASIN and week information
-    }
-});
 
-
-
-function downloadCSV(csvData, startDate, endDate, ASIN) {
-    const blob = new Blob([csvData], { type: 'text/csv' });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-
-    const filename = `${startDate}-${endDate}-${ASIN}.csv`;
-
-
-
-    a.setAttribute('hidden', '');
-    a.setAttribute('href', url);
-    a.setAttribute('download', filename); // Use the formatted filename
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+// Function to get current week number
+function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
 }
 
-function getMarketplaceSuffix(callback) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // Ensure you have at least one result before accessing the array
-        if (tabs[0]) {
-            var url = new URL(tabs[0].url);
-            var hostname = url.hostname;
+const currentWeekNumber = getWeekNumber(new Date());
+const startDate = new Date("2023-01-01");
 
-            if (hostname.includes('.com')) callback('US');
-            else if (hostname.includes('.co.uk')) callback('UK');
-            else if (hostname.includes('.de')) callback('DE');
-            else callback(null); // or a default value
+function jsonToCSV(jsonData, week, asin, marketplace, localCSVData) {
+    const rows = jsonData.reportsV2[0].rows;
+    rows.forEach(row => {
+        Object.keys(row).forEach(key => {
+            // Replace the field's value with 0 only if it is exactly a dash
+            if (row[key] === '-') {
+                row[key] = '0';
+            }
+        });
+
+        // Update additional data for each row
+        row['week'] = week;
+        row['asin'] = asin;
+        row['marketplace'] = marketplace;
+
+        // Specifically escape the 'Search Query' field for CSV if it contains commas, double quotes, or single quotes
+        if (row['qp-asin-query'] && (row['qp-asin-query'].includes(',') || row['qp-asin-query'].includes('"') || row['qp-asin-query'].includes("'"))) {
+            // Escape double quotes by doubling them, escape single quotes by replacing them with two single quotes (standard SQL way, might vary based on your needs) and wrap the value in double quotes
+            row['qp-asin-query'] = `"${row['qp-asin-query'].replace(/"/g, '""').replace(/'/g, "''")}"`;
         }
     });
+
+    // Sort rows based on 'qp-asin-query-rank'
+    rows.sort((a, b) => parseInt(a['qp-asin-query-rank']) - parseInt(b['qp-asin-query-rank']));
+
+    // Accumulate processed rows
+    localCSVData.push(...rows);
 }
 
-// Using the function with a callback because chrome.tabs.query is asynchronous
-getMarketplaceSuffix(function(suffix) {
-    updateMarketplaceDisplay(suffix); // Update display upon window load
+async function fetchData(asin, weekEndDate, marketplace, localCSVData) {
+    console.log(marketplace);
+    const payload = {
+        viewId: "query-performance-asin-view",
+        filterSelections: [
+            { id: "asin", value: asin, valueType: "ASIN" },
+            { id: "reporting-range", value: "weekly", valueType: null },
+            { id: "weekly-week", value: weekEndDate, valueType: "weekly" }
+        ],
+        selectedCountries: [marketplace.toLowerCase()],
+        reportId: "query-performance-asin-report-table",
+        reportOperations: [
+            {
+                reportId: "query-performance-asin-report-table",
+                reportType: "TABLE",
+                pageNumber: 1,
+                pageSize: 100,
+                sortByColumnId: "qp-asin-query-rank",
+                ascending: true
+            }
+        ],
+    };
+
+    let baseUrl = 'https://sellercentral.amazon.';
+    switch (marketplace) {
+        case 'UK':
+            baseUrl += 'co.uk';
+            break;
+        case 'DE':
+            baseUrl += 'de';
+            break;
+        case 'US':
+        default:
+            baseUrl += 'com';
+    }
+
+    const url = `${baseUrl}/api/brand-analytics/v1/dashboard/query-performance/reports`;
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+    })
+        .then(response => response.json())
+        .then(data => {
+            jsonToCSV(data, weekEndDate, asin, marketplace, localCSVData);  // Changed this line
+        })
+        .catch(error => console.error("Fetch or Parsing failed: ", error));
+
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 6000 + 1000));
+}
+
+async function fetchAllData(asin, startDate, endDate, marketplace) {
+    let localCSVData = []; // Clearing the array for a new ASIN
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const totalWeeks = Math.ceil((end - start) / (7 * 24 * 60 * 60 * 1000));
+
+    let currentWeek = 0;
+    let currentStartDate = new Date(startDate);
+
+    for (let i = 0; i < totalWeeks; i++) {
+        // Calculate the end date of the week (Saturday)
+        const weekEndDate = new Date(currentStartDate);
+        const dayOfWeek = weekEndDate.getDay(); // Sunday = 0, Monday = 1, ..., Saturday = 6
+        const daysToAdd = 6 - dayOfWeek; // Number of days to add to reach Saturday
+        weekEndDate.setDate(currentStartDate.getDate() + daysToAdd);
+
+        // Format the week's end date into a string
+        const weekEndStr = `${weekEndDate.getFullYear()}-${String(weekEndDate.getMonth() + 1).padStart(2, '0')}-${String(weekEndDate.getDate()).padStart(2, '0')}`;
+
+        // Fetch data for the week
+        await fetchData(asin, weekEndStr, marketplace, localCSVData);
+
+        // Update progress
+        currentWeek++;
+        chrome.runtime.sendMessage({
+            type: "UPDATE_PROGRESS",
+            progress: (currentWeek / totalWeeks) * 100,
+            currentWeek: currentWeek,
+            totalWeeks: totalWeeks,
+            ASIN: asin,
+            week: weekEndStr // Use formatted weekEndStr for the week
+        });
+
+        // Move to the start of the next week
+        currentStartDate.setDate(currentStartDate.getDate() + 7);
+    }
+
+
+    const orderedApiNames = Object.keys(columnMapping);
+
+    // Use the ordered API names to rearrange each row
+    const reorderedRows = localCSVData.map(row => {
+        return orderedApiNames.map(name => row[name] || '').join(',');
+    });
+
+    // Use the real header names in the CSV
+    const realHeaderNames = Object.values(columnMapping).join(',');
+    reorderedRows.unshift(realHeaderNames);
+
+    const csvData = reorderedRows.join('\n');
+    chrome.runtime.sendMessage({type: "CSV_DATA", payload: csvData, startDate: startDate, endDate: endDate, ASIN: asin});
+
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'fetchData') {
+        const { asin, startDate, endDate, marketplace } = message;
+        fetchAllData(asin, startDate, endDate, marketplace);
+    }
 });
-
-function updateMarketplaceDisplay(marketplaceSuffix) {
-    const marketplaceDisplay = document.getElementById('marketplaceSuffix');
-    marketplaceDisplay.innerText = marketplaceSuffix;
-}
